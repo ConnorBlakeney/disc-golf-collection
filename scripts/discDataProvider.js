afa8701a207083577fb7d2ce6417f53f916724e2
Connor Blakeney <connorblakeney@yahoo.com>
2020-07-20 08:16:27 -0500

    first commit to branch

---
+++ scripts/discDataProvider.js
@@ -0,0 +1,24 @@
+export const discCollection = [
+  {
+    brand: "Discraft Buzzz",
+    type: "mid-range",
+    color: "pink",
+    weight: 177,
+  },
+
+  {
+    brand: "Axiom Envy",
+    type: "putter",
+    color: "white with blue rim",
+    weight: 172,
+  },
+
+  {
+    brand: "Innova Mako3",
+    type: "mid-range",
+    color: "blue",
+    weight: 172,
+  },
+]
+
+export const discSliced = () => discCollection.slice()
