35db1f76b34cfd138dad70efe8704d03eccaa874
Connor Blakeney <connorblakeney@yahoo.com>
2020-07-20 08:58:45 -0500

    began working on discList.js, specifically discList function

--- scripts/discList.js
+++ scripts/discList.js
@@ -1,2 +1,8 @@
 import { discSliced } from "./discDataProvider.js"
 import { discAsHTML } from "./disc.js"
+
+export const discList = () => {
+  const discArray = discSliced()
+
+  const contentTarget = document.querySelector(".discs")
+}
